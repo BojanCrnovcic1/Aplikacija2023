32a5447807dfcbc28e5e6843723c9c464000f3b9
Bojan Crnovcic <bojan969@gmail.com>
2023-11-14 18:21:11 +0100

    Implament entity

---
+++ entities/article-price.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Article } from "./article.entity";
+
+@Index("fk_article_price_id_article_id", ["articleId"], {})
+@Entity("article_price")
+export class ArticlePrice {
+  @PrimaryGeneratedColumn({
+    type: "int",
+    name: "article_price_id",
+    unsigned: true,
+  })
+  articlePriceId: number;
+
+  @Column({type: "int",  name: "article_id", unsigned: true })
+  articleId: number;
+
+  @Column({
+    type: "decimal", 
+    unsigned: true,
+    precision: 10,
+    scale: 2,
+  })
+  price: number;
+
+  @Column({type: "timestamp",  name: "create_at", default: () => "'now()'" })
+  createAt: Date;
+
+  @ManyToOne(() => Article, (article) => article.articlePrices, {
+    onDelete: "RESTRICT",
+    onUpdate: "CASCADE",
+  })
+  @JoinColumn([{ name: "article_id", referencedColumnName: "articleId" }])
+  article: Article;
+}
