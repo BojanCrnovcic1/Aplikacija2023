b5d743733c2cd5e87470f6af90d1024922fc1e9a
Bojan Crnovcic <bojan969@gmail.com>
2023-11-14 10:52:11 +0100

    administrator service i frst run app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administrator.entity';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 
 @Controller()
 export class AppController {
+  constructor(private adminService : AdministratorService) {}
 
   @Get()
   getHello(): string {
     return "heloo";
+  }
+
+  @Get('api/administrator')
+  getAllAdmins(): Promise<Administrator[]> {
+    return this.adminService.getAll();
   }
 }
